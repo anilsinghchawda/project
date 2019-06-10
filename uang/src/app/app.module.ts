49f48d780c0ce9e13bacac6ee78ad03ed0b8c486
anilsinghchawda <anilsinghchawda@gmail.com>
2019-06-10 18:12:42 +0530

    component

---
+++ uang/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrandComponent } from './brand/brand.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ImageComponent } from './image/image.component';
+import { CatnavComponent } from './catnav/catnav.component';
+import { ProductComponent } from './product/product.component';
+import { EachproComponent } from './eachpro/eachpro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BrandComponent,
+    NavbarComponent,
+    ContentComponent,
+    FooterComponent,
+    HomeComponent,
+    ImageComponent,
+    CatnavComponent,
+    ProductComponent,
+    EachproComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ uang/src/app/catnav/catnav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catnav',
+  templateUrl: './catnav.component.html',
+  styleUrls: ['./catnav.component.scss']
+})
+export class CatnavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
